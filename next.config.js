916cbfc7bf2da92c1200f47ec230736ccbb76e9c
lee jeongmin <danmin20@khu.ac.kr>
2021-06-09 13:40:30 +0900

    chore: next setting

---
+++ next.config.js
@@ -0,0 +1,38 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  target: "serverless",
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+
+  webpack(conf) {
+    conf.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  // Enable figma's wrong mask-type attribute work
+                  removeRasterImages: false,
+                  removeStyleElement: false,
+                  removeUnknownsAndDefaults: false,
+                  // Enable svgr's svg to fill the size
+                  removeViewBox: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
+    // 절대경로
+    conf.resolve.modules.push(__dirname);
+    return conf;
+  },
+});
